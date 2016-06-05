234ee8abad174773050a973f70e1e165283b0e4a
Edwin Ojeda <edwin.ojeda@baruchmail.cuny.edu>
2016-06-05 19:13:48 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+    num1/num2
 end
 
 def assign_variable(value)
-  
+    name=value
 end
 
-def argue
-  
+def argue(variable)
+    variable
 end
 
-def greeting
+def greeting(variable1, variable2)
 
 end
 
 def return_a_value
+  ph="Nice"
 
 end
 
 def last_evaluated_value
-
+    ph="expert"
 end
 
-def pizza_party
-
+def pizza_party(pizza="cheese")
+    pizza
 end
